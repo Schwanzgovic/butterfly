a1e2fc2a64894ac497b9984526c186a78ec44345
uiteg <uiteg@student.kit.edu>
2026-09-03 14:47:46 +0200

    normalized butterfly distribution function to obtain empirical density of butterfly sightings; added some ideas how to follow up as comments

--- butterfly.py
+++ butterfly.py
@@ -3,14 +3,27 @@ import pandas as pd
 #import scikit-learn as sklearn
 import matplotlib.pyplot as plt
 
-def observationCounter(array):
-    observations = ()
+#wrong atm change later "Antal" is strings
+def totalObservationCounter(array):
+    observations = 0
     for i in array:
-        if type(i) == 'int':
-            observations += i
+        if  i == "noterad":
+            observations += 1
         else:
-            observations += 1 
+            observations += int(i) 
     return observations 
+
+#replace all "noterad" by a one 
+def observationToInt(array):
+    intObservations = np.ones(len(array))
+    for i in range(len(array)):
+        if array[i] == "noterad":
+            continue
+        else: 
+            intObservations[i] = int(array[i])
+    return intObservations
+            
+
 
 def extractMonth(array):
     monthArray = []
@@ -27,10 +40,34 @@ print(df)
 x = extractMonth(df["Startdatum"])
 observationsPerMonth = np.zeros(12)
 
-for i in x:
-    observationsPerMonth[int(i) - 1] += 1
+#for i in x:
+ #   observationsPerMonth[int(i) - 1] += 1
 print(x)
-#y = observationCounter(df["Antal"])
-plt.plot(observationsPerMonth)
-plt.show()
 
+#observations per month with actual values
+#for i in range(len(x)):
+#    observationsPerMonth[int(x[i]) - 1] += 
+y = observationToInt(df["Antal"])
+totalObservations = totalObservationCounter(df["Antal"])
+empiricalDensity = y/totalObservations
+
+print(empiricalDensity)
+#print(df["Antal"])
+print(y)
+
+#note that the extra information in the number of observed butterflys doesnt seem to change the distribution
+#model idea: assume underlying probability density and approxamite it by empirical density function (observationsPerMonth/totalObservations)
+
+#new idea: pca to find good predictors (but predictors for what??)
+
+#another idea: scatter plot with geographical positions (bigger dots reprensent more sightings) and colour them according to specific (climate?) zones
+
+for i in range(len(x)):
+    observationsPerMonth[int(x[i]) - 1] += y[i]
+
+plt.plot(observationsPerMonth)
+
+#scatter plot showing the sightings on the 'map'
+#plt.scatter(df["Ost"], df["Nord"])
+
+plt.show()
